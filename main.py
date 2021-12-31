8f17ccaff1b453ecf15824639e635a429b2fc740
Abdelrahman Abounida <abdelrahmanaboneda@gmail.com>
2021-12-31 10:41:26 +0200

    update main

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import string
 import pandas as pd
 #from postscrape.postscrape.spiders.NFL import NFL
-
 import pandas as pd
 
+# I found that I forgot to convert years type from string to list which makes its appearance not that good on excel 
+# so this is a simple pretty algorithm
+
 data = pd.read_csv("postscrape/postscrape/spiders/Newplayers.csv")
-d = data.Year
-
-x = data
-for i,l in enumerate(d):
+for i,years in enumerate(data):
     try:
-        x.iloc[i,1] = l.split(',')
+        data.iloc[i,1] = years.split(',')
     except:
-        x.iloc[i,1] = " "
+        data.iloc[i,1] = []
 
-print(x.iloc[:,1])
-x.to_csv ('pretty.csv', index = False, header=True)
+print(data.iloc[:,1])
+data.to_csv ('pretty.csv', index = False, header=True)
 
 
 
