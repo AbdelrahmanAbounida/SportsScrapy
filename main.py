1f5c603e0fb7ebdd91c2ba706ca1e9be65c58403
Abdelrahman Abounida <abdelrahmanaboneda@gmail.com>
2021-12-31 12:10:26 +0200

    update main method to filter some players

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import pandas as pd
 #from postscrape.postscrape.spiders.NFL import NFL
 
 import pandas as pd
-
+import re
 data = pd.read_csv("postscrape/postscrape/spiders/Newplayers.csv")
 d = data.Year
 
 x = data
 for i,l in enumerate(d):
     try:
-        x.iloc[i,1] = l.split(',')
+        lis = list(set(re.findall('....',l.replace(',',''))))
+        if('0000' in lis):
+            lis.remove('0000')
+        x.iloc[i,1] = lis
     except:
-        x.iloc[i,1] = " "
+        x.drop(labels=i,inplace=False,axis=0)
 
 print(x.iloc[:,1])
-x.to_csv ('pretty.csv', index = False, header=True)
+x.to_csv ('pretty2.csv', index = False, header=True)
 
 
 
